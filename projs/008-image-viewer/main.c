91644730a386b3d4c89fc48ae0ee5ad5394c902a
high-horse <akhileshadhikaria1@gmail.com>
2026-01-12 00:54:46 +0545

    ppm p6 image viewer

--- projs/008-image-viewer/main.c
+++ projs/008-image-viewer/main.c
@@ -2,48 +2,68 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
-int main() {
-    SDL_Init(SDL_INIT_VIDEO);
-    const int width = 900;
-    const int height = 600;
-    const char *ptitle = "Image Viewer";
 
-    // SDL_Window *pwindow =  SDL_CreateWindow(ptitle, width, height, 0);
+int main() 
+{
+    printf("hello world \n");
+    FILE *in = stdin;
+    int width = 900;
+    int height = 600;
+    
+    char *pthrowaway = calloc(1000, sizeof(char));
+    // Read first line (specifier p3 or p6)
+    fgets(pthrowaway, 1000, in);
+    free(pthrowaway);
 
-    SDL_Window  *pwindow = SDL_CreateWindow(ptitle, SDL_WINDOWPOS_CENTERED,  SDL_WINDOWPOS_CENTERED, width, height, 0) ;
-    SDL_Renderer *prenderer =  SDL_CreateRenderer(pwindow, -1, 0);
-    if(!pwindow || !prenderer)
+    char *pdimensions = calloc(1000, sizeof(char));
+    // read 2nd line  (dimensions)
+    fgets(pdimensions, 1000, in);
+    sscanf(pdimensions, "%d %d", &width, &height);
+    free(pdimensions);
+
+    char *pthrowable = calloc(1000, sizeof(char));
+    // read 2nd line  (max color)
+    fgets(pthrowable, 1000, in);
+    free(pthrowable);
+
+
+    SDL_Window *pwindow = SDL_CreateWindow("Image Viewer",  SDL_WINDOWPOS_CENTERED,  SDL_WINDOWPOS_CENTERED, width, height, 0);
+    SDL_Surface *psurface =  SDL_GetWindowSurface(pwindow); 
+    
+    int x, y;
+    x = y = 50;
+    SDL_Rect pixel = (SDL_Rect) {x, y, 1, 1};
+    Uint32 color =  0;
+
+    for (int i = 0; i < width; i++)
     {
-        SDL_Log("Failed to create window or renderer");
-        return EXIT_FAILURE;
+        for (int j = 0; j < width; j++)
+        {
+            Uint8 r, g, b;
+            r = (char) getchar();
+            g = (char) getchar();
+            b = (char) getchar();
+            color = SDL_MapRGB(psurface->format, r,  g,  b);
+            pixel.x = i;
+            pixel.y = j;
+            SDL_FillRect(psurface, &pixel, color);
+        }
+        
     }
-
-    // SDL_Rect *fillRect = { 50, 50, 100, 100 };
-    // const SDL_Rect *prect = {0, 0 , width, height}
-    // int SDL_RenderDrawRect(prenderer, fillRect);
-
-    // SDL_SetRenderDrawColor(prenderer, 255, 255, 255, 255);
-    // SDL_RenderClear(prenderer); // Fills the entire rendering target with white
     
-    // 2. Set the color for the rectangle (e.g., to blue)
-    SDL_SetRenderDrawColor(prenderer, 255, 0, 255, 255);
-    
-    // 3. Define the rectangle's position and size
-    SDL_Rect blueRect = { 150, 150, 10, 10 };
-    
-    // 4. Draw the filled blue rectangle
-    SDL_RenderFillRect(prenderer, &blueRect);
-    
-    // 5. Present the final rendered image to the window
-    SDL_RenderPresent(prenderer);
-    
-
-    SDL_Event event;
-    while(SDL_WaitEvent(&event))
-        if(event.type == SDL_QUIT) break;
-
-    SDL_DestroyWindow(pwindow);
-    SDL_Quit();
-    
+    SDL_FillRect(psurface, &pixel, color);
+    SDL_UpdateWindowSurface(pwindow);
+    // SDL_Delay(4000);
+    int running = 1;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {  // poll until all events are handled!
+            if(event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+        SDL_Delay(100);
+        // update game state, draw the current frame
+    }
     return EXIT_SUCCESS;
 }
