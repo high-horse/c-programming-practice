4d70da4161e1236bcc251b39147d22f4ac0e9c93
high-horse <akhileshadhikaria1@gmail.com>
2026-01-18 16:56:08 +0545

    change

--- projs/009-mouse-tracker/main.c
+++ projs/009-mouse-tracker/main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
     int fd = open(openingfile, O_RDONLY, 0);
     if (fd < 0)
     {
-        printf("ERROR OPENING FILE DISCRIPTOR");
+        printf("ERROR OPENING FILE DISCRIPTOR \n%d\n", fd);
         return EXIT_FAILURE;
     }
     printf("Successfulluy opened the file discriptorb %d\n", fd);
@@ -40,10 +40,44 @@ int main(int argc, char *argv[])
             break; // Exit the loop on error
         }
 
+        if (captured_event.type == EV_KEY)
+        {
+            // EV_KEY events are for key presses and releases
+            printf("Key event: type %u, code %u, value %u (0: release, 1: press, 2: autorepeat)\n",
+                   captured_event.type, captured_event.code, captured_event.value);
+            // You can use the codes defined in <linux/input-event-codes.h> for meaningful output
+        }
+        else if (captured_event.type == EV_REL)
+        {
+            // EV_REL events are for relative movements (e.g., mouse movement)
+            printf("Relative event: type %u, code %u, value %u (e.g., REL_X, REL_Y)\n",
+                   captured_event.type, captured_event.code, captured_event.value);
+        }
+        else if (captured_event.type == EV_MSC)
+        {
+            // EV_REL events are for relative movements (e.g., mouse movement)
+            printf("Relative event: type %u, code %u, value %u (e.g., REL_X, REL_Y)\n",
+                   captured_event.type, captured_event.code, captured_event.value);
+        }
+        else if (captured_event.type == EV_ABS)
+        {
+            // EV_REL events are for relative movements (e.g., mouse movement)
+            printf("Relative event EV_ABS: type %u, code %u, value %u (e.g., REL_X, REL_Y)\n",
+                   captured_event.type, captured_event.code, captured_event.value);
+        }
+        else if (captured_event.type == EV_SND)
+        {
+            // EV_REL events are for relative movements (e.g., mouse movement)
+            printf("Relative event: type %u, code %u, value %u (e.g., REL_X, REL_Y)\n",
+                   captured_event.type, captured_event.code, captured_event.value);
+        }
+
+        // EV_KEY, EV_SW, EV_LED, EV_SND
+
         // Use %zd for ssize_t
         printf("read resp %zd, event type %u, code %u\n",
                red, captured_event.type, captured_event.code);
-               reading = 0;
+        reading = 0;
     }
     // int x ;
     // char c;
