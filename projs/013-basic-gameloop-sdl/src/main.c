858e9279890fcdb86da1ff095b5faff63e6de95f
akhilesh <akhileshadhikaria1@gmail.com>
2026-02-04 23:00:14 +0545

    fix: add square

--- projs/013-basic-gameloop-sdl/src/main.c
+++ projs/013-basic-gameloop-sdl/src/main.c
@@ -8,6 +8,15 @@ int game_is_running = false;
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 
+struct Ball{
+    float x;
+    float y;
+    float width;
+    float height;
+};
+
+struct Ball ball;
+
 int initialize_window(void)
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
@@ -63,20 +72,45 @@ void process_input(void)
     }
 }
 
+
+void setup(void) {
+    ball.x = 20;
+    ball.y = 20;
+    ball.width = 15;
+    ball.height = 15;
+}
+
 void update(void)
 {
+    ball.x += 1;
+    ball.y += 1;
+     
 }
 
 void render(void)
 {
-    SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0 ,255);
     SDL_RenderClear(renderer);
+
+    // Draw a rectangle
+    SDL_Rect ball_rect = {
+        (int) ball.x, 
+        (int) ball.y, 
+        (int) ball.width, 
+        (int) ball.height
+    };
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    SDL_RenderFillRect(renderer, &ball_rect);
+
     SDL_RenderPresent(renderer);
+
+    // SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
+    // SDL_RenderClear(renderer);
+    // SDL_RenderPresent(renderer);
 }
 
-void setup(void) {
-
-}
 
 void destroy_window(void)
 {
