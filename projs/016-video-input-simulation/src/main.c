1667e909f6860e55d1b08940e3e7faee648d973b
akhilesh <akhileshadhikaria1@gmail.com>
2026-02-09 23:55:12 +0545

    start

--- projs/016-video-input-simulation/src/main.c
+++ projs/016-video-input-simulation/src/main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
-    (void) argc;
-    (void) argv;
+    if(argc != 2) {
+        printf("usage %s </dev/media_file_name>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char *filename = argv[1];
+    int fd = open(filename, O_RDWR);
+    if(fd < 0) {
+        perror("FAILED TO OPEN FILE DESCRIPTOR:");
+        return EXIT_FAILURE;
+    }
+
+    printf("File desc got %d\n", fd);
+    
 
     printf("hello world\n");
 
