41f3755376375010f72b3d1ae68f746a0bc0d7ea
akhilesh <akhileshadhikaria1@gmail.com>
2026-02-21 19:50:31 +0545

    http bare

--- networking/http/server/main.c
+++ networking/http/server/main.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
+#include <unistd.h>
 
 
 #define PORT 8080
@@ -17,9 +17,12 @@ int main()
 {
 	int socket_fd, error , new_socket;
 	int opt = 1;
-	struct sockaddr_in address;
-	char buffer[BUFFER_SIZE] = {0};
 
+	struct sockaddr_in address;
+	socklen_t address_len = sizeof(address);
+	char buffer[BUFFER_SIZE] = {0};
+	
+	// socket fd
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if(socket_fd == -1) {
 		perror("SOCKET FAILED");
@@ -27,47 +30,61 @@ int main()
 	}
 
 
-	// atach port to socket
+	// atach socket to port
 	error = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt));
 	if(error == -1) {
 		perror("SETSOCKET ADDR FAILED");
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT); 
 
+
+	// attach socket to the port
+	error = bind(socket_fd, (struct sockaddr *) &address, sizeof(address));
+	if(error < 0) {
+		perror("BIND FAILED");
+		exit(EXIT_FAILURE);
+	}
 	printf("Socket bond to port %d\n", PORT);
 
 
-	error = bind(socket_fd, (struct sockaddr *) &address, sizeof(address));
-	if(error != 0) {
-		perror("BIND FAILED");
-		return EXIT_FAILURE;
-	}
-
 	error = listen(socket_fd, 3);
-	if(error !=0 ) {
+	if(error < 0) {
 		perror("LISTEN FAILED");
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
+	}
+	
+	printf("Waiting for connections...\n");
+
+	while(true) {
+
+		// new_socket = accept(socket_fd, (struct sockaddr *)&address, sizeof(address));
+		new_socket = accept(socket_fd, (struct sockaddr *)&address, &address_len);
+
+		if(new_socket < 0) {
+			perror("ACCEPT FAILED");
+			exit(EXIT_FAILURE);
+		}
+
+		// subtract 1 for the null
+		// terminator at the end
+		ssize_t read_size = read(new_socket, buffer, BUFFER_SIZE-1 );
+		if(read_size < 0) {
+			perror("READ FAILED");
+			exit(EXIT_FAILURE);
+		}
+		printf("Recieved message : %s\n", buffer);
+		char *response = "hello world from server";
+		send(new_socket, response, strlen(response), 0);
+		printf("response sent: %s\n", response);
+		printf("==============================\n");
+
+		close(new_socket);
 	}
 
-	new_socket = accept(socket_fd, (struct sockaddr *)&address, sizeof(address));
-	if(new_socket == -1) {
-		perror("ACCEPT FAILED");
-		return EXIT_FAILURE;
-	}
-
-	// subtract 1 for the null
-    // terminator at the end
-	size_t read_size = read(new_socket, buffer, BUFFER_SIZE-1 );
-	printf("Recieved message : %s\n", buffer);
-	char *response = "hello world";
-	send(new_socket, response, strlen(response), 0);
-	printf("response sent\n");
-
-	close(new_socket);
 	close(socket_fd);
 	return EXIT_SUCCESS;
 }
