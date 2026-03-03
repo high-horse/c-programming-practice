6e3ffdccfd5a640099d9d2f01042aa183b6743b5
akhilesh <akhileshadhikaria1@gmail.com>
2026-03-03 21:17:38 +0545

    thread poling

--- networking/http/v4-multi-thread-multi-files/main.c
+++ networking/http/v4-multi-thread-multi-files/main.c
@@ -9,12 +9,11 @@
 #include <netinet/in.h>
 #include <time.h>
 #include <unistd.h>
-#include "lib/map.h"
 
-#define PORT 9990
+#define PORT 9999
 #define BACKLOG 100
 #define BUFFER_SIZE 1024
-hashmap * route_file;
+#define THREAD_POOL_SIZE 50
 
 typedef struct{
     char filename[256];
@@ -24,29 +23,65 @@ typedef struct{
     char version[16];
 }ThreadData;
 
+typedef struct Node{
+    ThreadData *data;
+    struct Node *next;
+} Node ;
 
-void * handle_client_old(int * client_fd, const char *filename) {
-    // int *client_fd = (int *)arg;
-    // printf("serving file %s\n", filename);
-    FILE *html = fopen(filename, "r");
-    if(html == NULL) {
-        perror("could not open file");
-        return NULL;
-    }
-    char read_buffer[BUFFER_SIZE] = {0};
-    size_t read_size = 0;
-    char *header =  "HTTP/1.1 200 OK\r\n"
-           "Content-Type: text/html\r\n"
-           "Connection: close\r\n"
-           "\r\n";
-    
-    send(*client_fd, header, strlen(header), 0);
-    
-    while ((read_size = fread(read_buffer, 1, BUFFER_SIZE, html)) > 0) {
-        send(*client_fd, read_buffer, read_size, 0);
-    }
-    return NULL;
+typedef struct {
+    Node *front;
+    Node *rear;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+} ThreadQueue;
+
+void initQueue(ThreadQueue *q) {
+    q->front = NULL;
+    q->rear = NULL;
+    pthread_mutex_init(&(q->mutex), NULL);
+    pthread_cond_init(&(q->cond), NULL);
 }
+
+void enque(ThreadQueue *q, ThreadData *data){
+    Node *newNode = malloc(sizeof(Node));
+    if(!newNode) return;
+    
+    newNode->data = data;
+    newNode->next = NULL;
+    
+    pthread_mutex_lock(&q->mutex);
+    
+    if(q->rear == NULL) {
+        q->front = newNode;
+        q->rear = newNode;
+    } else {
+        q->rear->next = newNode;
+        q->rear = newNode;
+    }
+    
+    pthread_cond_signal(&q->cond); 
+    pthread_mutex_unlock(&q->mutex);
+}
+
+ThreadData *deque(ThreadQueue *q) {
+    pthread_mutex_lock(&q->mutex);
+    while(q->front ==NULL) {
+        pthread_cond_wait(&q->cond, &q->mutex);
+    }
+    
+    Node *temp = q->front;
+    ThreadData *data = temp->data;
+    
+    q->front = temp->next;
+    if(q->front == NULL) {
+        q->rear = NULL;
+    }
+    free(temp);
+    pthread_mutex_unlock(&q->mutex);
+    return data;
+}
+
+
 
 char *parse_route(const char * route) {
     printf(" route %s\n", route);
@@ -59,9 +94,11 @@ char *parse_route(const char * route) {
     return "static/404.html";
 }
 
-void * handle_client(void *arg) {
+// void * handle_client(void *arg) {
+void * handle_client(ThreadData *thread_data) {
+    
     sleep(1);
-    ThreadData *thread_data = (ThreadData *)arg;
+    // ThreadData *thread_data = (ThreadData *)arg;
     strcpy(thread_data->filename, parse_route(thread_data->route));
     printf("parsed filename %s\n", thread_data->filename);
     FILE *html = fopen(thread_data->filename, "r");
@@ -89,6 +126,15 @@ void * handle_client(void *arg) {
     return NULL;
 }
 
+void *worker(void *arg) {
+    ThreadQueue *queue = (ThreadQueue *)arg;
+    while (true) {
+        ThreadData *thread_data = deque(queue);
+        handle_client(thread_data);
+    }
+    return NULL;
+}
+
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if(server_fd < 0) {
@@ -112,6 +158,16 @@ int main() {
         exit(EXIT_FAILURE);
     }
     printf("Server running on %d. Waiting for connection...\n", PORT);
+    
+    // thread pooling 
+    ThreadQueue queue;
+    initQueue(&queue);
+    pthread_t workers[THREAD_POOL_SIZE];
+    printf("Created thread pool of %d \n", THREAD_POOL_SIZE);
+    for (int i = 0; i < THREAD_POOL_SIZE; i++) {
+        pthread_create(&workers[i], NULL, worker, &queue);
+    }
+    
     while (true) {
         struct sockaddr_in client_address;
         socklen_t client_length = sizeof(client_address);
@@ -121,7 +177,7 @@ int main() {
             perror("Failed to accept client");
             continue;
         }
-        printf("connected ");
+        // printf("connected ");
         int bytes = recv(client_fd, request_buffer, BUFFER_SIZE, 0);
         if (bytes < 0) {
             close(client_fd);continue;
@@ -133,8 +189,9 @@ int main() {
  
         sscanf(request_buffer, "%s %s %s", thread_data->method, thread_data->route, thread_data->version);
         
-        pthread_t new_conn;
-        pthread_create(&new_conn, NULL, handle_client, (void *)thread_data);
+        enque(&queue, thread_data);
+        // pthread_t new_conn;
+        // pthread_create(&new_conn, NULL, handle_client, (void *)thread_data);
         // handle_client(thread_data);
     }
     
