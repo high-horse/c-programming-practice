e45f9511299810092faab91e2ad31ca7047f68ec
high-horse <akhileshadhikaria1@gmail.com>
2026-04-01 23:13:46 +0545

    grey color

--- projs/022-digital-clock-raylib/src/main.c
+++ projs/022-digital-clock-raylib/src/main.c
@@ -3,18 +3,36 @@
 #include <time.h>
 #include <stdbool.h>
 
-
 #include "raylib.h"
 
 #define W_WIDTH 800
 #define W_HEIGHT 400
 #define FPS 20
 
-#define THICKNESS 25
-#define LENGTH 80
+#define THICKNESS 20
+#define LENGTH 60
 #define TIP 15
+#define OFFSET 35
 
-void paint_triangle_strip(Vector2 center, bool vertical) {
+#define DARKESTGRAY CLITERAL(Color){ 20, 20, 20, 255 }  // Dark gray
+#define ON_COLOR GREEN
+#define OFF_COLOR DARKESTGRAY
+
+// top_left, top, top_right, bottom_right, bottom, bottom_left, middle
+int numbers[10][7] = {
+  {true, true, true, true, true, true, false}, // 0
+  {false, false, true, true, false, false, false}, // 1
+  {false, true, true, false, true, true, true}, // 2
+  {false, true, true, true, true, false, true}, // 3
+  {true, false, true, true, false, false, false}, // 4
+  {true, true, false, true, true, false, true}, // 5
+  {true, true, false, true, true, true, true}, // 6
+  {false, true, true, false, false, false, false}, // 7
+  {true, true, true, true, true, true, true}, // 8
+  {true, true, true, true, true, false, true}, // 9
+};
+
+void paint_triangle_strip(Vector2 center, bool vertical, Color color) {
     Vector2 a, b ,c ,d, e, f;
     int count = 6;
     
@@ -35,53 +53,58 @@ void paint_triangle_strip(Vector2 center, bool vertical) {
     }
    
     Vector2 points[]  = {a, c, b, f, d, e};
-    DrawTriangleStrip(points, count, GREEN);
+    DrawTriangleStrip(points, count, color);
 }
 
 
 void draw_digit_(int digit) {
-    Vector2 center = (Vector2){W_WIDTH / 2, W_HEIGHT / 2};
+    Vector2 center = {W_WIDTH / 2, W_HEIGHT / 2};
+    int *color_flags = numbers[digit];
+    float h = LENGTH;
+    float off = OFFSET;
 
     // Horizontal segments
-    Vector2 top    = {center.x, center.y - LENGTH};
+    Vector2 top    = {center.x, center.y - h - off};
     Vector2 middle = {center.x, center.y};
-    Vector2 bottom = {center.x, center.y + LENGTH};
+    Vector2 bottom = {center.x, center.y + h + off};
 
-    // Vertical segments
-    Vector2 topLeft     = {center.x - LENGTH / 2, center.y - LENGTH / 2};
-    Vector2 topRight    = {center.x + LENGTH / 2, center.y - LENGTH / 2};
-    Vector2 bottomLeft  = {center.x - LENGTH / 2, center.y + LENGTH / 2};
-    Vector2 bottomRight = {center.x + LENGTH / 2, center.y + LENGTH / 2};
+    // Vertical segments (perfectly aligned grid)
+    Vector2 top_left     = {center.x - h/2 - off, center.y - h/2};
+    Vector2 top_right    = {center.x + h/2 + off, center.y - h/2};
+    Vector2 bottom_left  = {center.x - h/2 - off, center.y + h/2};
+    Vector2 bottom_right = {center.x + h/2 + off, center.y + h/2};
 
-    // Draw a "7" shape (your target)
-    paint_triangle_strip(top, false);        // top horizontal
-    paint_triangle_strip(topLeft, true);    // right vertical
+    paint_triangle_strip(top_left, true, color_flags[0] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(top, false, color_flags[1] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(top_right, true, color_flags[2] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(bottom_right, true, color_flags[3] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(bottom, false, color_flags[4] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(bottom_left, true, color_flags[5] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(middle, false, color_flags[6] ? ON_COLOR : OFF_COLOR);
 }
 
 void draw_digit(int digit) {
+    int *color_flags = numbers[digit];
+    
     Vector2 center = (Vector2){W_WIDTH / 2, W_HEIGHT / 2};
     
-    Vector2 top_left = {center.x - LENGTH / 2, center.y - LENGTH/2}; 
-    
-    Vector2 top = {center.x , center.y - LENGTH};
-    
-    Vector2 top_right = (Vector2){center.x + LENGTH / 2 , center.y - LENGTH /2};
-    Vector2 bottom_right = (Vector2){center.x + LENGTH / 2 , center.y + LENGTH /2};
-    Vector2 bottom = (Vector2){center.x , center.y + LENGTH };
-    Vector2 bottom_left = (Vector2){center.x - LENGTH / 2 , center.y + LENGTH / 2};
+    Vector2 top = {center.x , center.y - LENGTH - OFFSET};
     Vector2 middle = (Vector2){center.x , center.y};
+    Vector2 bottom = (Vector2){center.x , center.y + LENGTH + OFFSET};
     
-    paint_triangle_strip(top_left, true);
-    paint_triangle_strip(top, false);
-    paint_triangle_strip(top_right, true);
-    paint_triangle_strip(bottom_right, true);
-    paint_triangle_strip(bottom, false);
-    paint_triangle_strip(bottom_left, true);
-    paint_triangle_strip(middle, false);
+    Vector2 top_left = {center.x - LENGTH / 2  - OFFSET/2, center.y - LENGTH/2 - OFFSET /2}; 
+    Vector2 top_right = (Vector2){center.x + LENGTH / 2 + OFFSET/2, center.y - LENGTH/2 - OFFSET /2};
+    Vector2 bottom_right = (Vector2){center.x + LENGTH / 2 + OFFSET/2, center.y + LENGTH /2 + OFFSET /2};
+    Vector2 bottom_left = (Vector2){center.x - LENGTH / 2 - OFFSET/2, center.y + LENGTH / 2 + OFFSET / 2};
+   
     
-    
-    
-    
+    paint_triangle_strip(top_left, true, color_flags[0] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(top, false, color_flags[1] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(top_right, true, color_flags[2] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(bottom_right, true, color_flags[3] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(bottom, false, color_flags[4] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(bottom_left, true, color_flags[5] ? ON_COLOR : OFF_COLOR);
+    paint_triangle_strip(middle, false, color_flags[6] ? ON_COLOR : OFF_COLOR);
     
 }
 
@@ -94,8 +117,7 @@ int main() {
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        // draw_rectangle();
-        draw_digit(1);
+        draw_digit(2);
         EndDrawing();
     }
     
